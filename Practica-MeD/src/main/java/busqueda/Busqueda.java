2b9bdfc920689bfbc81be71853970438677e151c
Xoán <Xokage@users.noreply.github.com>
2015-10-08 08:50:15 +0000

    Subimos o proxecto en Eclipse

---
+++ Practica-MeD/src/main/java/busqueda/Busqueda.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.busqueda;
+
+import java.util.List;
+
+/**
+ *
+ * @author Urist
+ */
+public class Busqueda {
+
+    private Long id;
+    private List<FiltroXeral> filtros;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<FiltroXeral> getFiltros() {
+        return filtros;
+    }
+
+    public void setFiltros(List<FiltroXeral> filtros) {
+        this.filtros = filtros;
+    }
+
+}
