6867c6f8d80d47a93fbe50003297e2e95cdd9980
xoan.antelo <xoan.antelo@0d7250f9-7fae-440c-9090-7d59c61ffd0e>
2015-10-13 20:32:47 +0000

    Fix

--- Practica-MeD/src/main/java/hotel/AbstractSqlHotelDao.java
+++ Practica-MeD/src/main/java/hotel/AbstractSqlHotelDao.java
@@ -8,9 +8,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-import es.udc.ws.app.model.answer.Answer;
-import es.udc.ws.app.model.event.Event;
-
 public abstract class AbstractSqlHotelDao implements SqlHotelDao {
 	
 	public Hotel getHotel(Connection connection, Long id) {
@@ -85,47 +82,61 @@ public abstract class AbstractSqlHotelDao implements SqlHotelDao {
 
             while (resultSet.next()) {
 
-                int i = 1;
+                i = 1;
                 
-                Long tmpEventId = resultSet.getLong(i++);
-                String name = resultSet.getString(i++);
-                String description = resultSet.getString(i++);
-                Calendar tmpStartDate = Calendar.getInstance();
-                tmpStartDate.setTime(resultSet.getTimestamp(i++));
-                Calendar tmpFinishDate = Calendar.getInstance();
-                tmpFinishDate.setTime(resultSet.getTimestamp(i++));
-
-                int capacity = resultSet.getInt(i++);
-                boolean isInternal = resultSet.getBoolean(i++);
-                int attendantNum = resultSet.getInt(i++);
-                String facebookId = resultSet.getString(i++);
+                Long newId = resultSet.getLong(i++); 
+                String newNome= resultSet.getString(i++);
+                String newLocalizacion = resultSet.getString(i++);
+                String newDescricion = resultSet.getString(i++);
+                Integer newCategoria = resultSet.getInt(i++);
+                Calendar newTemporadaInicio = Calendar.getInstance();
+                newTemporadaInicio.setTime(resultSet.getTimestamp(i++));
+                Calendar newTemporadaFin = Calendar.getInstance();
+                newTemporadaFin.setTime(resultSet.getTimestamp(i++));
+                String newServizos = resultSet.getString(i++);
+                String newTelefono = resultSet.getString(i++);
                 
                 
-                events.add(new Event(tmpEventId, name, description, tmpStartDate, tmpFinishDate,capacity,isInternal, attendantNum,facebookId));
+                
+                hoteis.add(new Hotel(newId, newNome, newLocalizacion, newDescricion, newCategoria, newTemporadaInicio,
+                		newTemporadaFin, newServizos, newTelefono));
 
             }
             
-            /* Get results. */
-            i = 1;
-            Long newId = resultSet.getLong(i++); 
-            String newNome= resultSet.getString(i++);
-            String newLocalizacion = resultSet.getString(i++);
-            String newDescricion = resultSet.getString(i++);
-            Integer newCategoria = resultSet.getInt(i++);
-            Calendar newTemporadaInicio = Calendar.getInstance();
-            newTemporadaInicio.setTime(resultSet.getTimestamp(i++));
-            Calendar newTemporadaFin = Calendar.getInstance();
-            newTemporadaFin.setTime(resultSet.getTimestamp(i++));
-            String newServizos = resultSet.getString(i++);
-            String newTelefono = resultSet.getString(i++);
-            
-            return new Hotel(newId, newNome, newLocalizacion, newDescricion, newCategoria, newTemporadaInicio,
-            		newTemporadaFin, newServizos, newTelefono);
+            /* Return hoteis. */
+            return hoteis;
             
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
-		return null;
 	}
+
+
+    public void delHotel(Connection connection, Long hotelId) {
+    	
+    	/* Create "queryString". */
+        String queryString = "DELETE FROM Hotel WHERE" + " id = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(queryString)) {
+
+            /* Fill "preparedStatement". */
+            int i = 1;
+            preparedStatement.setLong(i++, hotelId);
+
+            /* Execute query. */
+            int removedRows = preparedStatement.executeUpdate();
+
+            if (removedRows == 0) {
+            	System.err.println("Hotel id:" + hotelId + " non atopado.");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    
+
+
+
 }
