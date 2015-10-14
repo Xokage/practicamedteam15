be19e614427977eefb3b7348a9ccf86cc5b15d62
Xoán <Xokage@users.noreply.github.com>
2015-10-14 15:30:50 +0000

    Por fin funciona o jdbc. Esto si que foi unha liada.

--- Practica-MeD/src/main/java/busqueda/BusquedaServiceImpl.java
+++ Practica-MeD/src/main/java/busqueda/BusquedaServiceImpl.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Calendar;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 import main.java.util.DataSourceLocator;
--- Practica-MeD/src/main/java/rest/BusquedaRest.java
+++ Practica-MeD/src/main/java/rest/BusquedaRest.java
@@ -3,10 +3,16 @@ package main.java.rest;
 import static main.java.util.ModelConstants.BUSQUEDA_DATA_SOURCE;
 
 import java.sql.Connection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 import javax.sql.DataSource;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -15,6 +21,7 @@ import javax.ws.rs.QueryParam;
 
 import main.java.busqueda.Busqueda;
 import main.java.busqueda.BusquedaService;
+import main.java.busqueda.BusquedaServiceImpl;
 import main.java.busqueda.SqlBusquedaDao;
 import main.java.busqueda.SqlBusquedaDaoFactory;
 import main.java.hotel.Hotel;
@@ -22,15 +29,41 @@ import main.java.hotel.Hotel;
 @Path("busqueda")
 public class BusquedaRest {
 
-	BusquedaService busquedaService;
+	BusquedaService busquedaService = new BusquedaServiceImpl();
+	SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy");
 
 	@GET
-	@Produces(MediaType.TEXT_XML)
-	public String buscarXML(@QueryParam("localizacion") String localizacion,
-			@QueryParam("dataInicio") Calendar dataInicio,
-			@QueryParam("dataFin") Calendar dataFin,
-			@DefaultValue("1") @QueryParam("numPersoas") int numPersoas,
-			@DefaultValue("0") @QueryParam("opcion") int opcion) {
+	@Produces(MediaType.TEXT_PLAIN)
+	public String buscarXML(@Context UriInfo ui) {
+		MultivaluedMap<String, String> queryParams = ui.getQueryParameters();
+		String localizacion = queryParams.getFirst("localizacion");
+		Calendar dataInicio = Calendar.getInstance();
+		if (queryParams.getFirst("dataInicio") != null) {
+			try {
+				dataInicio
+						.setTime(sdf.parse(queryParams.getFirst("dataInicio")));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		} else dataInicio = null;
+		Calendar dataFin = Calendar.getInstance();
+
+		if (queryParams.getFirst("dataFin") != null) {
+			try {
+				dataFin.setTime(sdf.parse(queryParams.getFirst("dataFin")));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		} else dataFin = null;
+		
+		int numPersoas = 1;
+		if(queryParams.getFirst("numPersoas")!=null){
+			numPersoas = Integer.parseInt(queryParams.getFirst("numPersoas"));
+		}
+		int opcion = 0; 
+		if(queryParams.getFirst("opcion")!=null){
+			numPersoas = Integer.parseInt(queryParams.getFirst("opcion"));
+		}
 
 		Busqueda busqueda = busquedaService.realizarBusqueda(localizacion,
 				dataInicio, dataFin, numPersoas, opcion);
