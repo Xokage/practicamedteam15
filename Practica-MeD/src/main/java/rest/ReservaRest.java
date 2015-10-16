667cda65427553f1218e9fd071505c1df5462e79
xoan.antelo <xoan.antelo@0d7250f9-7fae-440c-9090-7d59c61ffd0e>
2015-10-16 19:56:02 +0000

    Commit de entrega. Funcionan os tests e as historias están implementadas

--- Practica-MeD/src/main/java/rest/ReservaRest.java
+++ Practica-MeD/src/main/java/rest/ReservaRest.java
@@ -4,66 +4,19 @@ import main.java.model.db.reserva.Reserva;
 import main.java.model.service.reserva.ReservaService;
 import main.java.model.service.reserva.ReservaServiceImpl;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.POST;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import java.util.Calendar;
-
-import static main.java.util.ModelConstants.BUSQUEDA_DATA_SOURCE;
-import static org.junit.Assert.*;
-
-import java.util.Calendar;
-
-import javax.sql.DataSource;
-import javax.ws.rs.core.Application;
-
-import main.java.model.db.habitacion.Habitacion;
-import main.java.model.db.hotel.Hotel;
-import main.java.model.service.habitacion.HabitacionService;
-import main.java.model.service.habitacion.HabitacionServiceImpl;
-import main.java.model.service.hotel.HotelService;
-import main.java.model.service.hotel.HotelServiceImpl;
-import main.java.rest.BusquedaRest;
-import main.java.util.DataSourceLocator;
-import main.java.util.SimpleDataSource;
-
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.test.JerseyTest;
-import org.glassfish.jersey.test.TestProperties;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
 
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-
-import main.java.model.db.busqueda.Busqueda;
-import main.java.model.db.filtro.Filtro;
-import main.java.model.db.habitacion.Habitacion;
-import main.java.model.db.hotel.Hotel;
-import main.java.model.service.busqueda.BusquedaService;
-import main.java.model.service.busqueda.BusquedaServiceImpl;
-import main.java.model.service.filtro.FiltroService;
-import main.java.model.service.filtro.FiltroServiceImpl;
-import main.java.util.Pair;
 
 @Path("reserva")
 public class ReservaRest {
@@ -107,18 +60,18 @@ public class ReservaRest {
 		Reserva reserva = reservaService.reservar(nomeCliente, dniCliente,
 				dataEntrada, dataSaida, idHotel, idHabitacion);
 
-		String result = "<?xml version=\"1.0\"?>" 
-				+ "<reserva>"
-					+ "<id>" + reserva.getId() + "</id>" 
-					+ "<nomeCliente>" + reserva.getNomeCliente() + "</nomeCliente>"
-					+ "<dniCliente>" + reserva.getDniCliente() + "</dniCliente>" 
-					+ "<dataEntrada>" + sdf.format(reserva.getDataEntrada().getTime()) + "</dataEntrada>"
-					+ "<dataSaida>" + sdf.format(reserva.getDataSaida().getTime()) + "</dataSaida>"
-					+ "<idHotel>" + reserva.getIdHotel() + "</idHotel>"
-					+ "<idHabitacion>" + reserva.getIdHabitacion() + "</idHabitacion>"
-				+ "</reserva>"; 
+		String result = "<?xml version=\"1.0\"?>" + "<reserva>" + "<id>"
+				+ reserva.getId() + "</id>" + "<nomeCliente>"
+				+ reserva.getNomeCliente() + "</nomeCliente>" + "<dniCliente>"
+				+ reserva.getDniCliente() + "</dniCliente>" + "<dataEntrada>"
+				+ sdf.format(reserva.getDataEntrada().getTime())
+				+ "</dataEntrada>" + "<dataSaida>"
+				+ sdf.format(reserva.getDataSaida().getTime()) + "</dataSaida>"
+				+ "<idHotel>" + reserva.getIdHotel() + "</idHotel>"
+				+ "<idHabitacion>" + reserva.getIdHabitacion()
+				+ "</idHabitacion>" + "</reserva>";
 
 		return result;
-		
+
 	}
 }
--- Practica-MeD/src/test/java/rest/ReservarJerseyTest.java
+++ Practica-MeD/src/test/java/rest/ReservarJerseyTest.java
@@ -7,12 +7,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import javax.sql.DataSource;
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
-import junit.framework.Assert;
 import main.java.model.db.habitacion.Habitacion;
 import main.java.model.db.hotel.Hotel;
 import main.java.model.db.reserva.Reserva;
@@ -22,7 +18,6 @@ import main.java.model.service.hotel.HotelService;
 import main.java.model.service.hotel.HotelServiceImpl;
 import main.java.model.service.reserva.ReservaService;
 import main.java.model.service.reserva.ReservaServiceImpl;
-import main.java.rest.BusquedaRest;
 import main.java.rest.ReservaRest;
 import main.java.util.DataSourceLocator;
 import main.java.util.SimpleDataSource;
@@ -33,7 +28,6 @@ import org.glassfish.jersey.test.JerseyTest;
 import org.glassfish.jersey.test.TestProperties;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
 
 public class ReservarJerseyTest extends JerseyTest {
 
@@ -55,95 +49,105 @@ public class ReservarJerseyTest extends JerseyTest {
 		DataSourceLocator.addDataSource(BUSQUEDA_DATA_SOURCE, dataSource);
 
 	}
-	
+
 	@Override
-    protected Application configure() {
-        // Find first available port.
+	protected Application configure() {
+		// Find first available port.
 		ResourceConfig rs = new ResourceConfig(ReservaRest.class);
-        forceSet(TestProperties.CONTAINER_PORT, "0");
-        rs.register(MultiPartFeature.class);
-        return rs;
-    }
-	
+		forceSet(TestProperties.CONTAINER_PORT, "0");
+		rs.register(MultiPartFeature.class);
+		return rs;
+	}
+
 	@Test
-	public void testReservarHabitacion()  {
-		//Inicializacion dos datos
-		
-	    String nome = "Hotel Soneira";
-	    String localizacion = "Vimianzo";
-	    String descricion = "";
-	    int categoria = 5;
-	    Calendar temporadaInicio = Calendar.getInstance();
-	    Calendar temporadaFin = Calendar.getInstance();
-	    temporadaFin.add(Calendar.MONTH, 3);
-	    String servizos = "wifi,piscina,cocktail";
-	    String telefono = "618382641";
-		
-		final Hotel newHotel = hs.addHotel(nome, localizacion, descricion, categoria, 
-				temporadaInicio, temporadaFin, servizos, telefono);
-		
-	    Float prezoHab = 300f;
-	    int numCamasHab = 2;
-		
-		final Habitacion newHabitacion = has.addHabitacion(prezoHab, numCamasHab, newHotel.getId());
-		
-	    String nome2 = "Hotel California";
-	    String localizacion2 = "Vimianzo";
-	    String descricion2 = "";
-	    int categoria2 = 5;
-	    Calendar temporadaInicio2 = Calendar.getInstance();
-	    Calendar temporadaFin2 = Calendar.getInstance();
-	    temporadaFin2.add(Calendar.MONTH, 3);
-	    String servizos2 = "wifi,piscina,cocktail";
-	    String telefono2 = "618382641";
-		
-		final Hotel newHotel2 = hs.addHotel(nome2, localizacion2, descricion2, categoria2, 
-				temporadaInicio2, temporadaFin2, servizos2, telefono2);
-	    
-	    Float prezoHab2 = 400f;
-	    int numCamasHab2 = 2;
-		
-		final Habitacion newHabitacion2 = has.addHabitacion(prezoHab2, numCamasHab2, newHotel2.getId());
-		
-		
-		//Parametros individuais
+	public void testReservarHabitacion() {
+		// Inicializacion dos datos
+
+		String nome = "Hotel Soneira";
+		String localizacion = "Vimianzo";
+		String descricion = "";
+		int categoria = 5;
+		Calendar temporadaInicio = Calendar.getInstance();
+		Calendar temporadaFin = Calendar.getInstance();
+		temporadaFin.add(Calendar.MONTH, 3);
+		String servizos = "wifi,piscina,cocktail";
+		String telefono = "618382641";
+
+		final Hotel newHotel = hs.addHotel(nome, localizacion, descricion,
+				categoria, temporadaInicio, temporadaFin, servizos, telefono);
+
+		Float prezoHab = 300f;
+		int numCamasHab = 2;
+
+		final Habitacion newHabitacion = has.addHabitacion(prezoHab,
+				numCamasHab, newHotel.getId());
+
+		String nome2 = "Hotel California";
+		String localizacion2 = "Vimianzo";
+		String descricion2 = "";
+		int categoria2 = 5;
+		Calendar temporadaInicio2 = Calendar.getInstance();
+		Calendar temporadaFin2 = Calendar.getInstance();
+		temporadaFin2.add(Calendar.MONTH, 3);
+		String servizos2 = "wifi,piscina,cocktail";
+		String telefono2 = "618382641";
+
+		final Hotel newHotel2 = hs.addHotel(nome2, localizacion2, descricion2,
+				categoria2, temporadaInicio2, temporadaFin2, servizos2,
+				telefono2);
+
+		Float prezoHab2 = 400f;
+		int numCamasHab2 = 2;
+
+		final Habitacion newHabitacion2 = has.addHabitacion(prezoHab2,
+				numCamasHab2, newHotel2.getId());
+
+		// Parametros individuais
 		Calendar dataEntrada = Calendar.getInstance();
 		dataEntrada.add(Calendar.MONTH, 1);
 		Calendar dataSaida = Calendar.getInstance();
 		dataEntrada.add(Calendar.MONTH, 2);
 
-	    Reserva reserva = new Reserva();
-	    reserva.setDataEntrada(dataEntrada);
-	    reserva.setDataSaida(dataSaida);
-	    reserva.setDniCliente("12345678C");
-	    reserva.setIdHabitacion(newHabitacion2.getId());
-	    reserva.setIdHotel(newHotel2.getId());
-	    reserva.setNomeCliente("Pepiño");
-		String expectedResult = "<?xml version=\"1.0\"?>" 
-				+ "<reserva>"
-					+ "<id>" + reserva.getId() + "</id>" 
-					+ "<nomeCliente>" + reserva.getNomeCliente() + "</nomeCliente>"
-					+ "<dniCliente>" + reserva.getDniCliente() + "</dniCliente>" 
-					+ "<dataEntrada>" + sdf.format(reserva.getDataEntrada().getTime()) + "</dataEntrada>"
-					+ "<dataSaida>" + sdf.format(reserva.getDataSaida().getTime()) + "</dataSaida>"
-					+ "<idHotel>" + reserva.getIdHotel() + "</idHotel>"
-					+ "<idHabitacion>" + reserva.getIdHabitacion() + "</idHabitacion>"
-				+ "</reserva>"; 
-		
-	    final String testResult = target("reserva/add").queryParam("dataEntrada",sdf.format(dataEntrada.getTime()))
-		.queryParam("dataSaida",sdf.format(dataSaida.getTime())).queryParam("dniCliente","12345678C")
-		.queryParam("idHabitacion",newHabitacion2.getId()).queryParam("idHotel", newHotel2.getId())
-		.queryParam("nomeCliente","Pepiño").request().get(String.class);
-	    Reserva r = new Reserva(reserva);
-	    //Limpar Datos
-		System.out.println(r);
-	    rs.delReserva(r.getId());
+		Reserva reserva = new Reserva();
+		reserva.setDataEntrada(dataEntrada);
+		reserva.setDataSaida(dataSaida);
+		reserva.setDniCliente("12345678C");
+		reserva.setIdHabitacion(newHabitacion2.getId());
+		reserva.setIdHotel(newHotel2.getId());
+		reserva.setNomeCliente("Pepiño");
+
+		final String testResult = target("reserva/add")
+				.queryParam("dataEntrada", sdf.format(dataEntrada.getTime()))
+				.queryParam("dataSaida", sdf.format(dataSaida.getTime()))
+				.queryParam("dniCliente", "12345678C")
+				.queryParam("idHabitacion", newHabitacion2.getId())
+				.queryParam("idHotel", newHotel2.getId())
+				.queryParam("nomeCliente", "Pepiño").request()
+				.get(String.class);
+
+		Reserva r = new Reserva(reserva);
+		r.setId(Long.parseLong(testResult.substring(
+				testResult.indexOf("<id>") + 4, testResult.indexOf("</id>"))));
+
+		String expectedResult = "<?xml version=\"1.0\"?>" + "<reserva>"
+				+ "<id>" + r.getId() + "</id>" + "<nomeCliente>"
+				+ reserva.getNomeCliente() + "</nomeCliente>" + "<dniCliente>"
+				+ reserva.getDniCliente() + "</dniCliente>" + "<dataEntrada>"
+				+ sdf.format(reserva.getDataEntrada().getTime())
+				+ "</dataEntrada>" + "<dataSaida>"
+				+ sdf.format(reserva.getDataSaida().getTime()) + "</dataSaida>"
+				+ "<idHotel>" + reserva.getIdHotel() + "</idHotel>"
+				+ "<idHabitacion>" + reserva.getIdHabitacion()
+				+ "</idHabitacion>" + "</reserva>";
+
+		// Limpar Datos
+		rs.delReserva(r.getId());
 		has.delHabitacion(newHabitacion.getId());
 		has.delHabitacion(newHabitacion2.getId());
 		hs.delHotel(newHotel.getId());
 		hs.delHotel(newHotel2.getId());
 
-	    assertEquals(expectedResult,testResult);
+		assertEquals(expectedResult, testResult);
 	}
 
 }
