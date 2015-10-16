83f52da434e6fa6dd1addb5025ab315d67b3b235
Cristian Canosa <cristiancanosa@users.noreply.github.com>
2015-10-16 17:12:41 +0000

    Engadimos busqueda por datas e polo nome do Cliente nas reservas Cambiamos un metodo a getReservas, que se obten reserva a partir do id

--- Practica-MeD/src/main/java/model/db/reserva/AbstractSqlReservaDao.java
+++ Practica-MeD/src/main/java/model/db/reserva/AbstractSqlReservaDao.java
@@ -5,10 +5,16 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Calendar;
+import java.sql.Statement;
+import java.sql.Timestamp;
+
+import java.util.ArrayList;
+import java.util.List;
+import main.java.model.db.reserva.Reserva;
 
 public abstract class AbstractSqlReservaDao implements SqlReservaDao {
 
-	public Reserva findReserva(Connection connection, Long idReserva) {
+	public Reserva getReserva(Connection connection, Long idReserva) {
 
 		/* Create "queryString". */
 		String queryString = "SELECT id, dataReserva, nomeCliente, DniCliente, dataEntrada, dataSaida, idHotel, idHabitacion, FROM Reserva "
@@ -51,7 +57,132 @@ public abstract class AbstractSqlReservaDao implements SqlReservaDao {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	public List<Reserva> findReservaByClient(Connection connection, String nomeCliente) {
 
+		/* Create "queryString". */
+		String queryString = "SELECT id, dataReserva, nomeCliente, DniCliente, dataEntrada, dataSaida, idHotel, idHabitacion, FROM Reserva "
+				+ "WHERE nomeCliente = ?";
+
+		try (PreparedStatement preparedStatement = connection
+				.prepareStatement(queryString)) {
+
+			/* Fill "preparedStatement". */
+			int i = 1;
+			preparedStatement.setString(i++, nomeCliente);
+
+			/* Execute query. */
+			ResultSet resultSet = preparedStatement.executeQuery();
+
+			if (!resultSet.next()) {
+				// If you find nothing return null
+				return null;
+			}
+
+			/* Get results. */
+			
+			List<Reservas> reservas = new ArrayList<Reservas>();
+			
+			
+			
+			while (resultSet.next()) {
+				
+				i = 1;
+				Long newId = resultSet.getLong(i++);
+				Calendar newDataReserva = Calendar.getInstance();
+				newDataReserva.setTime(resultSet.getTimestamp(i++));
+				String newNomeCliente = resultSet.getString(i++);
+				String newDniCliente = resultSet.getString(i++);
+				Calendar newDataEntrada = Calendar.getInstance();
+				newDataEntrada.setTime(resultSet.getTimestamp(i++));
+				Calendar newDataSaida = Calendar.getInstance();
+				newDataSaida.setTime(resultSet.getTimestamp(i++));
+				Long newIdHotel = resultSet.getLong(i++);
+				Long newIdHabitacion = resultSet.getLong(i++);
+
+				return reservas.add(new Reserva(newId, newDataReserva, newNomeCliente,
+						newDniCliente, newDataEntrada, newDataSaida, newIdHotel,
+						newIdHabitacion));
+			}
+			
+			
+			return reservas;
+			
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public Reserva findReservaByDates(Connection connection, Long idHabitacion, Calendar dataEntrada, Calendar dataSaida) {
+
+		/* Create "queryString". */
+		String queryString = "SELECT id, dataReserva, nomeCliente, DniCliente, dataEntrada, dataSaida, idHotel, idHabitacion, FROM Reserva WHERE idHabitacion = ?";
+		
+		if ((dataEntrada != null) && (dataSaida != null)) {
+			
+		}
+		
+		if (dataEntrada != null) {
+			queryString += " AND dataEntrada = ?";
+			
+		} 
+		
+		if (dataSaida != null) {
+			queryString += " AND dataSaida = ?";
+		}
+
+		try (PreparedStatement preparedStatement = connection
+				.prepareStatement(queryString)) {
+
+			/* Fill "preparedStatement". */
+			int i = 1;
+			
+			preparedStatement.setLong(i++, idHabitacion);
+			
+			if (dataEntrada != null) {
+				preparedStatement.setTimestamp(i++,
+						dataEntrada != null ? new Timestamp(dataEntrada
+								.getTime().getTime()) : null);
+				
+			} 
+			
+			if (dataSaida != null) {
+				preparedStatement.setTimestamp(i++,
+						dataSaida != null ? new Timestamp(dataSaida
+								.getTime().getTime()) : null);
+			}
+
+			/* Execute query. */
+			ResultSet resultSet = preparedStatement.executeQuery();
+
+			if (!resultSet.next()) {
+				// If you find nothing return null
+				return null;
+			}
+
+			/* Get results. */
+			i = 1;
+			Long newId = resultSet.getLong(i++);
+			Calendar newDataReserva = Calendar.getInstance();
+			newDataReserva.setTime(resultSet.getTimestamp(i++));
+			String newNomeCliente = resultSet.getString(i++);
+			String newDniCliente = resultSet.getString(i++);
+			Calendar newDataEntrada = Calendar.getInstance();
+			newDataEntrada.setTime(resultSet.getTimestamp(i++));
+			Calendar newDataSaida = Calendar.getInstance();
+			newDataSaida.setTime(resultSet.getTimestamp(i++));
+			Long newIdHotel = resultSet.getLong(i++);
+			Long newIdHabitacion = resultSet.getLong(i++);
+
+			return new Reserva(newId, newDataReserva, newNomeCliente,
+					newDniCliente, newDataEntrada, newDataSaida, newIdHotel,
+					newIdHabitacion);
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
 	public void delReserva(Connection connection, Long id) {
 
 		/* Create "queryString". */
--- Practica-MeD/src/main/java/model/db/reserva/SqlReservaDao.java
+++ Practica-MeD/src/main/java/model/db/reserva/SqlReservaDao.java
@@ -3,10 +3,16 @@ package main.java.model.db.reserva;
 import java.sql.Connection;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+
 public interface SqlReservaDao {
 	
 
-	public Reserva findReserva(Connection connection, Long idReserva);
+	public Reserva getReserva(Connection connection, Long idReserva);
+	
+	public List<Reserva> findReservaByClient(Connection connection, String nomeCliente);
+	
+	public Reserva findReservaByDates(Connection connection, Calendar dataEntrada, Calendar dataSaida, Long idHabitacion);
 
 	public Reserva addReserva(Connection connection, Calendar dataReserva, String nomeCliente, String DniCliente, 
     		Calendar dataEntrada, Calendar dataSaida, Long idHotel, Long idHabitacion);
